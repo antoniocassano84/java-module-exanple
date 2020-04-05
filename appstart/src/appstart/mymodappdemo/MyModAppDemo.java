008a8620cd001b3defef7a0e33c7768b68553871
antoniocassano84 <antonio.cassano984@gmail.com>
2020-04-05 08:48:07 +0100

    try to get back from here3333

--- appstart/src/appstart/mymodappdemo/MyModAppDemo.java
+++ appstart/src/appstart/mymodappdemo/MyModAppDemo.java
@@ -6,7 +6,7 @@ import appfuncs.simplefuncs.SimpleMathFuncs;
 public class MyModAppDemo {
   public static void main(String[] args) {
 
-    System.out.println("ciao");
+    System.out.println("boh");
 
     if(SimpleMathFuncs.isFactor(2, 10)) System.out.println("2 is a factor of 10");
     System.out.println("Smallest factor common to both 35 and 105 is " +
