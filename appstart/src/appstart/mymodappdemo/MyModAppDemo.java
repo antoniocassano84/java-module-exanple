e9dda54369bd4699403548ba95b9aadf08425461
antoniocassano84 <antonio.cassano984@gmail.com>
2020-04-05 09:01:33 +0100

    service branch deleted

--- appstart/src/appstart/mymodappdemo/MyModAppDemo.java
+++ appstart/src/appstart/mymodappdemo/MyModAppDemo.java
@@ -5,9 +5,6 @@ import appfuncs.simplefuncs.SimpleMathFuncs;
 
 public class MyModAppDemo {
   public static void main(String[] args) {
-
-    System.out.println("service branch");
-
     if(SimpleMathFuncs.isFactor(2, 10)) System.out.println("2 is a factor of 10");
     System.out.println("Smallest factor common to both 35 and 105 is " +
             SimpleMathFuncs.lcf(35, 105));
